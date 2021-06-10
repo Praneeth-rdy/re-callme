69625298561d2b0a15742e3706c8392f3ffe324d
Praneeth Kolanu <k.praneeth1199@iitkgp.ac.in>
2021-06-11 00:12:17 +0530

    Modified the styling of layout, removed options component (moved to layout) and added environment vars to use local server when available

--- client/src/SocketContext.js
+++ client/src/SocketContext.js
@@ -4,9 +4,9 @@ import Peer from 'simple-peer';
 
 const SocketContext = createContext();
 
-// const socket = io('http://localhost:5050');
+const socket = io(process.env.REACT_APP_SERVER_URI || 'https://praneeth-video-call.glitch.me/');
 
-const socket = io('https://praneeth-video-call.glitch.me/');
+// const socket = io('https://praneeth-video-call.glitch.me/');
 
 const ContextProvider = ({ children }) => {
     const [stream, setStream] = useState(null);
--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from 'react';
-import { FaUser, FaVideo, FaMicrophone, FaPhone, FaEllipsisV } from 'react-icons/fa';
+import { FaUser, FaVideo, FaMicrophone, FaPhone, FaEllipsisV, FaClone } from 'react-icons/fa';
 
 import Styles from '../styles/layout.module.css';
 
@@ -7,40 +7,45 @@ import { SocketContext } from '../SocketContext';
 
 
 function Layout({ children }) {
-    const { me, callAccepted, name, setName, callEnded, leaveCall, callUser, answerCall, call } = useContext(SocketContext);
+    const { me, myVideo, callAccepted, name, setName, callEnded, leaveCall, callUser, userVideo, answerCall, call } = useContext(SocketContext);
     const [idToCall, setIdToCall] = useState('');
 
     return (
         <div className={Styles.bgContainer}>
             <header>
                 <h1>Call Me</h1>
-                <div className={Styles.headerIconContainer}>
-                    <FaUser />
-                </div>
-            </header>
-            { children}
-            <div className={Styles.footerContainer}>
-                <div>
-                    <input type="text" placeholder="Your Name" value={name} onChange={(e) => setName(e.target.value)} />
-                    <button onClick={() => navigator.clipboard.writeText(me)}>Copy ID</button>
-                </div>
                 {call.isReceivedCall && !callAccepted && (
                     <div>
                         {call.name} is callng:
                         <button onClick={answerCall}>Answer</button>
                     </div>
                 )}
-                <input type="text" placeholder="ID to call" value={idToCall} onChange={(e) => setIdToCall(e.target.value)} />
-                
+                <div className={Styles.headerIconContainer}>
+                    <FaUser />
+                </div>
+            </header>
+            { children}
+            <div className={Styles.footerContainer}>
+                <div className={Styles.inputDiv}>
+                    <input type="text" placeholder="Your Name" value={name} onChange={(e) => setName(e.target.value)} />
+                    <span className={Styles.footerIcon} onClick={() => navigator.clipboard.writeText(me)}><FaClone size={12} /></span>
+                </div>
+
+                {!(callAccepted && !callEnded) ? (
+                    <div className={Styles.inputDiv}>
+                        <input type="text" placeholder="ID to call" value={idToCall} onChange={(e) => setIdToCall(e.target.value)} />
+                        <span className={Styles.bgGreen + ' ' + Styles.footerIcon} onClick={() => callUser(idToCall)}><FaPhone size={12} /></span>
+                    </div>
+                ) : ('')}
+
+
                 <footer>
-                    <div><FaVideo /></div>
-                    <div><FaMicrophone /></div>
+                    <div className={Styles.footerIcon} onClick={() => {}}><FaVideo /></div>
+                    <div className={Styles.footerIcon} onClick={() => {}}><FaMicrophone /></div>
                     {callAccepted && !callEnded ? (
-                        <div className={Styles.bgRed} onClick={leaveCall}><FaPhone /></div>
-                    ) : (
-                        <div className={Styles.bgGreen} onClick={() => callUser(idToCall)}><FaPhone /></div>
-                    )}
-                    <div><FaEllipsisV /></div>
+                        <div className={Styles.bgRed + ' ' + Styles.footerIcon} onClick={leaveCall}><FaPhone /></div>
+                    ) : ('')}
+                    <div className={Styles.footerIcon}><FaEllipsisV /></div>
                 </footer>
             </div>
 
