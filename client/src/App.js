c5f02364a94526b2406bf73e4e87fdc3271e3bbd
Praneeth Kolanu <k.praneeth1199@iitkgp.ac.in>
2021-06-10 22:25:51 +0530

    Removed the options component and added the options to footerContainer of Layout.js component and added a predeploy script to the scripts

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,11 @@
 // import logo from './logo.svg';
 import VideoGrid from './components/VideoGrid.js';
-import Options from './components/Options.js';
 import Layout from './components/Layout.js';
 
 function App() {
   return (
     <Layout>
       <VideoGrid />
-      <Options />
     </Layout>
   );
 }
--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,22 +1,49 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import { FaUser, FaVideo, FaMicrophone, FaPhone, FaEllipsisV } from 'react-icons/fa';
 
-import styles from '../styles/layout.module.css';
+import Styles from '../styles/layout.module.css';
+
+import { SocketContext } from '../SocketContext';
+
 
 function Layout({ children }) {
+    const { me, callAccepted, name, setName, callEnded, leaveCall, callUser, answerCall, call } = useContext(SocketContext);
+    const [idToCall, setIdToCall] = useState('');
+
     return (
-        <div className={styles.bgContainer}>
+        <div className={Styles.bgContainer}>
             <header>
                 <h1>Call Me</h1>
-                <div className={styles.headerIconContainer}><FaUser /></div>
+                <div className={Styles.headerIconContainer}>
+                    <FaUser />
+                </div>
             </header>
-            { children }
-            <footer>
-                <div><FaVideo /></div>
-                <div><FaMicrophone /></div>
-                <div className={styles.bgRed} ><FaPhone /></div>
-                <div><FaEllipsisV /></div>
-            </footer>
+            { children}
+            <div className={Styles.footerContainer}>
+                <div>
+                    <input type="text" placeholder="Your Name" value={name} onChange={(e) => setName(e.target.value)} />
+                    <button onClick={() => navigator.clipboard.writeText(me)}>Copy ID</button>
+                </div>
+                {call.isReceivedCall && !callAccepted && (
+                    <div>
+                        {call.name} is callng:
+                        <button onClick={answerCall}>Answer</button>
+                    </div>
+                )}
+                <input type="text" placeholder="ID to call" value={idToCall} onChange={(e) => setIdToCall(e.target.value)} />
+                
+                <footer>
+                    <div><FaVideo /></div>
+                    <div><FaMicrophone /></div>
+                    {callAccepted && !callEnded ? (
+                        <div className={Styles.bgRed} onClick={leaveCall}><FaPhone /></div>
+                    ) : (
+                        <div className={Styles.bgGreen} onClick={() => callUser(idToCall)}><FaPhone /></div>
+                    )}
+                    <div><FaEllipsisV /></div>
+                </footer>
+            </div>
+
         </div>
     )
 }
